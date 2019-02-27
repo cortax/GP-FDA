2b8e545d8e617d16859c66392d4e4281efb889b9
PLGAG13 <pierre-louis.gagnon.1@ulaval.ca>
2019-02-27 16:55:46 -0500

    Ajout de nhgpsolver, intégration de nhgpmixture

--- code/development_and_testing_mixture.m
+++ code/development_and_testing_mixture.m
@@ -1,8 +1,12 @@
 x_timegrid = linspace(-1,1,500);
+n = 20;
+
+n1 = floor(n/2);
+n2 = n - n1;
 
 hyper = make_hyper();
 
-mixture = nhgpmixture(3, ...
+mixture = nhgpmixture(2, ...
     x_timegrid, ...
     hyper.mu_m, hyper.G_m, hyper.L_m, ...
     hyper.mu_loggamma, hyper.G_loggamma, hyper.L_loggamma, ...
@@ -10,34 +14,17 @@ mixture = nhgpmixture(3, ...
     hyper.mu_logeta, hyper.G_logeta, hyper.L_logeta, ...
     hyper.tol);
 
-prior = nhgpprior(x_timegrid, ...
-    hyper.mu_m, hyper.G_m, hyper.L_m, ...
-    hyper.mu_loggamma, hyper.G_loggamma, hyper.L_loggamma, ...
-    hyper.mu_loglambda, hyper.G_loglambda, hyper.L_loglambda, ...
-    hyper.mu_logeta, hyper.G_logeta, hyper.L_logeta, ...
-    hyper.tol);
+Y1 = array2timetable(groundtruth_model1.random(n1),'RowTimes',seconds(x_timegrid), 'VariableNames',"Y1_" + (1:n1));
+Y2 = array2timetable(groundtruth_model2.random(n2),'RowTimes',seconds(x_timegrid), 'VariableNames',"Y2_" + (1:n2));
 
-gp1 = nhgpmodel(x_timegrid, zeros(size(x_timegrid)), zeros(size(x_timegrid)),zeros(size(x_timegrid)),zeros(size(x_timegrid)));
-gp2 = nhgpmodel(x_timegrid, ones(size(x_timegrid)), ones(size(x_timegrid)),ones(size(x_timegrid)),ones(size(x_timegrid)));
+problem.data = [Y1,Y2];
+problem.mixture = mixture;
+problenm.expectactions = [repmat([1,0], 10,1);repmat([0,1], 10,1)];
 
-groundtruth_model1 = prior.random_nhgp();
-Y1 = array2timetable(groundtruth_model1.random(10),'RowTimes',seconds(x_timegrid), 'VariableNames',"Y1_" + (1:10));
-
-groundtruth_model2 = prior.random_nhgp();
-Y2 = array2timetable(groundtruth_model2.random(10),'RowTimes',seconds(x_timegrid), 'VariableNames',"Y2_" + (1:10));
-
-Y = [Y1,Y2];
-gps = [gp1,gp2];
+solver = nhgpsolver();
 
 for t = 1:100
-    E_step(Y,gps);
-    M_step(Y,gps);
+    solver.E_step(problem);
+    solver.M_step(problem);
 end
 
-function output = E_step(Y,gps)
-    output = [];
-end
-
-function output = M_step(Y, gps)
-    output = [];
-end
---
+++ code/nhgpsolver.m
@@ -0,0 +1,22 @@
+classdef nhgpsolver < handle
+    
+    properties
+        problem
+        
+    end
+    
+    methods
+        function obj = nhgpsolver(inputArg1,inputArg2)
+            
+        end
+        
+        function problem = E_step(obj,problem)
+            
+        end
+        
+        function problem = M_step(obj, problem)
+            
+        end
+    end
+end
+
